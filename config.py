48e1053a043cce27f14fcb0ab8473c6de789d764
杨雨晔 <18951998816@163.com>
2022-09-13 22:28:01 +0800

    initial version

---
+++ config.py
@@ -0,0 +1,9 @@
+from nonebot.default_config import *
+
+NICKNAME = {'儿子'}
+
+SUPERUSERS = {1052139044}
+COMMAND_START = {'', '/', '!', '／', '！'}
+
+TENCENT_BOT_SECRET_ID = 'AKIDHOIyPaYHl01mHWUCepkPoAJFwOaXNq32'
+TENCENT_BOT_SECRET_KEY = 'oMgkaAVaQ1NTKxLTMzUkkV0QRCiyzlHY'
